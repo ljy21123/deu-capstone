feb7dd96e1e3db76d21acc789d47c3693ae71283
준영 <128689935+ljy21123@users.noreply.github.com>
2024-06-13 02:01:23 +0900

    정렬 수정3 (키워드)

--- infoWEB/src/main/java/com/example/infoweb/repository/NounFrequencyRepository.java
+++ infoWEB/src/main/java/com/example/infoweb/repository/NounFrequencyRepository.java
@@ -10,7 +10,15 @@
 package com.example.infoweb.repository;
 
 import com.example.infoweb.entity.NounFrequency;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface NounFrequencyRepository extends CrudRepository<NounFrequency, Long> {
+
+    @Override
+    @Query("SELECT nf FROM NounFrequency nf WHERE nf.id IN (SELECT MIN(nf1.id) FROM NounFrequency nf1 GROUP BY nf1.noun) ORDER BY nf.frequency DESC")
+    List<NounFrequency> findAll();
+
 }
